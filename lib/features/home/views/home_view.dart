bcd682bcfa26bbad138fa16b979068fdd4d6acfa
jfushada wahyoo gitlab <jfushada@wahyoo.com>
2021-10-12 12:01:58 +0700

    feat: views and widgets

---
+++ lib/features/home/views/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  static const String routeName = 'home-view';
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
